e907c1020c07e9acb402e821dc46e6df9a821806
Patrick Dack <patrickdack89@gmail.com>
2021-04-22 18:46:27 -0300

    Exercicio 6 terminado.

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_6.js
@@ -1,4 +1,18 @@
-let piece ='KNIGHT';
-let lower = piece.toLowerCase();
-console.log(lower);
-//if (piece = pown)
+let piece ='';
+let lowerPiece = piece.toLowerCase();
+
+if (lowerPiece == 'king') {
+    console.log('One square in any direction.');
+} else if (lowerPiece == 'queen'){
+    console.log('Diagonally, horizontally or vertically.');
+} else if (lowerPiece == 'rook') {
+    console.log('Horizontally or vertically.');
+} else if (lowerPiece == 'bishop') {
+    console.log('Diagonally.');
+} else if (lowerPiece == 'knight') {
+    console.log('In an L shape.');
+} else if (lowerPiece == 'pawn') {
+    console.log('Vertically forward one square.');
+} else {
+    console.log('Error, ivalid piece.');
+}
