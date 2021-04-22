f6b0d49d7e62f30d652be75888e69a00d1d98938
Patrick Dack <patrickdack89@gmail.com>
2021-04-22 16:05:26 -0300

    Exercicio 1 resolvido.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -0,0 +1,11 @@
+let a = 23;
+let b = 9;
+console.log('O resultado de ' + a + '+' + b + ' é igual a: ', a+b);
+
+console.log('O resultado de ' + a + '-' + b + ' é igual a: ', a-b);
+
+console.log('O resultado de ' + a + '*' + b + ' é igual a: ', a*b);
+
+console.log('O resultado de ' + a + '/' + b + ' é igual a: ', a/b);
+
+console.log('O resultado de ' + a + '%' + b + ' é igual a: ', a%b);
