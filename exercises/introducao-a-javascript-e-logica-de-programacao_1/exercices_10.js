24897536ca4555ca15e5a5265b61f9d011c49728
Patrick Dack <patrickdack89@gmail.com>
2021-04-22 21:12:26 -0300

    Exercicio 10 resolvido.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercices_10.js
@@ -0,0 +1,10 @@
+let valorCusto = 100.00;
+let valorVenda = 150.00;
+let valorCustoTotal = valorCusto * 1.2;
+let lucro;
+let lucroTotal;
+lucro = valorVenda - valorCustoTotal
+lucroTotal = lucro * 1000;
+console.log(lucroTotal);
+
+
