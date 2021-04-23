5cca22212a733835f0b39c2cf3ede7802b143d4a
Patrick Dack <patrickdack89@gmail.com>
2021-04-23 01:06:39 -0300

    Exercicio 11 comcluido.

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercice_11.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercice_11.js
@@ -1,7 +1,8 @@
-let salarioBruto = 2500;
+let salarioBruto = 6000;
 let inss;
 let ir;
 let salarioLiquido;
+
 //Calculo do INSS.
 if (salarioBruto <= 1556.94) {
     inss = 0.08;
@@ -18,12 +19,19 @@ if (salarioBruto <= 1556.94) {
 }
 
 //Calculo do IR.
-
 if (salarioLiquido <= 1903.98) {
-    ir = 1;
+    ir = 0;
 } else if (1903.98 < salarioLiquido && salarioLiquido <= 2826.65) {
+    ir = salarioLiquido * 0.075 - 142.8;
+} else if (2826.65 < salarioLiquido && salarioLiquido <= 3751.05) {
     ir = salarioLiquido * 0.15 - 354.8;
+} else if (3751.05 < salarioLiquido && salarioLiquido <= 4664.68) {
+    ir = salarioLiquido * 0.225 - 636.13;
+} else {
+    ir = salarioLiquido * 0.275 - 869.36;
 }
+salarioLiquido = salarioLiquido - ir;
+console.log('O salário liquido é R$'+salarioLiquido);
 
 
 
