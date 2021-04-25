09824eac19cba5a31c369f99831d95eef64a87cb
Patrick Dack <patrickdack89@gmail.com>
2021-04-25 15:32:22 -0300

    Exercicio 1 resolvido.

---
+++ exercises/exercice_1.js
@@ -0,0 +1,11 @@
+let n = 100;
+
+for (let index0 = 0; index0 < n; index0 += 1) {
+    let out = '';
+    for (let index = 0; index < n; index +=1) {
+        out += '*'
+    }
+    console.log(out);
+}
+
+
