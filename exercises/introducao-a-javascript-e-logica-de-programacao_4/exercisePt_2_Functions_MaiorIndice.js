d2a6bdd84a220cd15fafbde4413619d99dc5d60a
Patrick Dack <patrickdack89@gmail.com>
2021-04-28 03:04:15 -0300

    Resolve o exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercisePt_2_Functions_MaiorIndice.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercisePt_2_Functions_MaiorIndice.js
@@ -1,17 +1,13 @@
-function maiorIndice (Array) {
-    let indice;
-    for(let index = 0; index < 1; index += 1){
-        for(let index2 = 0; index2 < Array.length; index2 += 1){
-            if(Array[index] > Array[index2]){
-                indice = index;
-            
-            } else {
-                indice = index2;
-            }
-        };
-    };
-    return indice;
+function maiorIndice (array) {
+   let maiorIndice = 0;
+   for (index in array){
+       if (array[maiorIndice] < array[index]){
+           maiorIndice = index;
+       };
+   };
+   return maiorIndice;
 };
 
-let array = [2222, 33, 61, 7800, 10, 123, 1700]; 
-console.log(maiorIndice(array));
+let numbers = [2, 3, 6, 7, 10, 1]; 
+
+console.log(maiorIndice(numbers));
