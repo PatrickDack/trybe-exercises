1b169da209460327dac498c732c8e6224899ee17
Patrick Dack <patrickdack89@gmail.com>
2021-05-03 19:06:56 -0300

    Exercicio 1 concluido, dias do mes criados.

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,19 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let daysMonth = document.getElementById('days');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let days = document.createElement('li');
+    days.innerText = dezDaysList[index];
+    if (dezDaysList[index] == 24 || dezDaysList[index] == 25 || dezDaysList[index] == 31) {
+        days.className = 'day holiday'
+    } else if (dezDaysList[index] == 4 || dezDaysList[index] == 11 || dezDaysList[index] == 18 || dezDaysList[index] == 25) {
+        days.className = 'day friday';
+    } else {
+        days.className = 'day';
+    }
+    daysMonth.appendChild(days);
+
+  }
+  
