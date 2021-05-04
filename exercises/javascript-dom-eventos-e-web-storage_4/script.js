733150b7be76dc2649ef6217d087021a795b750f
Patrick Dack <patrickdack89@gmail.com>
2021-05-04 20:22:50 -0300

    Cria as variaveis necessarias para recuperar os valores dos botoes e cria a regra de negocio para alterar a cor do fundo araves do evento de click.

---
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -0,0 +1,21 @@
+//Recuparação dos botoẽs que serão usados para os eventos.
+const btnBackground = document.getElementById('btn-background');
+const btnTextColor = document.getElementById('btn-text-color');
+const btnFontSize = document.getElementById('btn-font-size');
+const btnLineSpace = document.getElementById('btn-line-space');
+const btnFontType = document.getElementById('btn-font-type');
+
+//Altera a cor de fundo do texto e a cor do texto.
+btnBackground.addEventListener('click', event => {
+    let bg = document.querySelector('.text-conteiner');
+    let text = document.querySelector('#main-text');
+    if (bg.style.backgroundColor !== 'black' && text.style.color !== 'white') {
+        bg.style.backgroundColor = 'black';
+        text.style.color = 'white'
+    } else {
+        bg.style.backgroundColor = 'white';
+        text.style.color = 'black';
+    }
+})
+
+//Altera a cor do texto
