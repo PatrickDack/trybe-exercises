596bb6c6d8302612b2e6b2e59416634a7016f55b
Patrick Dack <patrickdack89@gmail.com>
2021-05-12 20:50:25 -0300

    Estilizaçao CSS com auxilio do bootstrap

--- exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
@@ -9,16 +9,23 @@ for (let item in states) {
   dropDown.appendChild(createOption);
 }
 
-const inputs = document.querySelectorAll('input');
-const textArea = document.querySelector('textarea');
-const clear = document.getElementById('clear-button');
-function clearField() {
-  clear.addEventListener('click', () => {
-    for (let index = 0; index < inputs.length; index += 1) {
-      inputs[index].value = '';
-      inputs[index].innerText = '';
-      textArea.value = '';
-    }
-  });
-}
-clearField();
+var picker = new Pikaday({
+  field: document.getElementById('input-date'),
+  format: 'DD/MM/YYYY',
+  toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+  }
+});
