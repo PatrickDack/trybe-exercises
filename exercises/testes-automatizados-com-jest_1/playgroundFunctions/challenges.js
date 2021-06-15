d2fa60152903e568ca5f833619085143d2052fcc
Patrick Dack <patrickdack89@gmail.com>
2021-06-15 04:20:33 -0300

    Exercicio 3 parte 2 - Refatoraçao e testes jest das funçoes techList e hydrate.

--- exercises/testes-automatizados-com-jest_1/playgroundFunctions/challenges.js
+++ exercises/testes-automatizados-com-jest_1/playgroundFunctions/challenges.js
@@ -1,65 +1,3 @@
-// Desafio 1
-function compareTrue(paran1, paran2) {
-  return paran1 && paran2;
-}
-
-// Desafio 2
-function calcArea(base, height) {
-  return (base * height) / 2;
-}
-
-// Desafio 3
-function splitSentence(sentence) {
-  let str = sentence;
-  return str.split(' ');
-}
-
-// Desafio 4
-function concatName(array) {
-  return array[array.length - 1].concat(',', ' ', array[0]);
-}
-
-// Desafio 5
-function footballPoints(wins, ties) {
-  let winPoints = 3;
-  return (winPoints * wins) + ties;
-}
-
-// Desafio 6
-function highestCount(array) {
-  array.sort((a, b) => a - b);
-  let count = 0;
-  for (let number of array) {
-    if (array[array.length - 1] === number) {
-      count += 1;
-    }
-  }
-  return count;
-}
-
-// Desafio 7
-function catAndMouse(unD0, unD1, unD2) {
-  let mouse = unD0;
-  let cat1 = unD1;
-  let cat2 = unD2;
-  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) return 'cat1';
-  if (Math.abs(mouse - cat2) < Math.abs(mouse - cat1)) return 'cat2';
-  return 'os gatos trombam e o rato foge';
-}
-
-// Desafio 8
-function fizzBuzz(array) {
-  let fizzBuzzArray = [];
-  for (let number of array) {
-    if (number % 3 === 0 && number % 5 === 0) fizzBuzzArray.push('fizzBuzz');
-    if (number % 3 === 0) fizzBuzzArray.push('fizz');
-    if (number % 5 === 0) fizzBuzzArray.push('buzz');
-    fizzBuzzArray.push('bug!');
-  }
-  return fizzBuzzArray;
-  }
-
-// Desafio 9
 function encode(string) {
   let stringToArray = string.split('');
   for (let index in stringToArray) {
@@ -72,7 +10,6 @@ function encode(string) {
   return stringToArray.join('');
 }
 
-console.log(encode('Patrick Dack'));
 function decode(string) {
   let stringToArray = string.split('');
   for (let index in stringToArray) {
@@ -85,16 +22,7 @@ function decode(string) {
   return stringToArray.join('');
 }
 
-
 module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
   decode,
   encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
 };
--- exercises/testes-automatizados-com-jest_1/playgroundFunctions/challenges2.js
+++ exercises/testes-automatizados-com-jest_1/playgroundFunctions/challenges2.js
@@ -1,86 +1,20 @@
-// Desafio 10
 function techList(array, name) {
-  if (array[0] === undefined) {
-    return 'Vazio!';
-  }
-  array.sort();
-  let newList = [];
-  for (let key in array) {
-    let object = {};
-    object.tech = array[key];
-    object.name = name;
-    newList.push(object); 
-  }
-  return newList;
+  if (array.length === 0) return 'Vazio!';
+
+  return array.sort().map(tech => ({
+    'tech': tech,
+    'name': name,
+  }));
 }
 
-// Desafio 11
-function generatePhoneNumber(arr) {
-  if (arr.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  for (let number of arr) {
-    if (number < 0 || number > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-  }
-  let repeatTimes = 0;
-  let count = 0;
-  for (let number of arr) { 
-    let compareNuber = number;
-    for (let number2 of arr) {
-      if (compareNuber === number2) {
-        count += 1;
-      }
-    }
-    if (count >= repeatTimes) {
-      repeatTimes = count;
-    }
-    count = 0;
-  } 
-  if (repeatTimes >= 3) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
-  arr.splice(0, 0, '(');
-  arr.splice(3, 0, ')');
-  arr.splice(4, 0, ' ');
-  arr.splice(10, 0, '-');
-  return arr.join('');
-}
-
-// Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-  let tLine = [lineA, lineB, lineC];
-  tLine.sort((a, b) => a - b);
-  if ((tLine[2] < tLine[0] + tLine[1]) && (tLine[2] > Math.abs(tLine[0] - tLine[1]))) {
-    return true;
-  } else {
-    return false;
-  } 
-}
-
-// Desafio 13
 function hydrate(string) {
-  let numbers = [];
-  for (let index = 0; index < string.length; index += 1) {
-    if (string[index] == 1 || string[index] == 2 || string[index] == 3 || string[index] == 4 || string[index] == 5 || string[index] == 6 || string[index] == 7 || string[index] == 8 || string[index] == 9) {
-      numbers.push(parseInt(string[index]));
-    }   
-  }
-  let total = 0;
-  for (let number of numbers) {
-    total += number;
-  } 
-  if (total === 1) {
-    return `${total} copo de água`;
-  } else {
-    return `${total} copos de água`;
-  }
+  const result = string.split('').map((char) => parseInt(char))
+  .reduce((cup, char) => isNaN(char) === false ? cup + char : cup, 0);
+  const phrase = result === 1 ? 'copo' : 'copos';
+  return `${result} ${phrase} de água`;
 }
 
 module.exports = {
-  generatePhoneNumber,
   techList,
   hydrate,
-  triangleCheck,
 };
