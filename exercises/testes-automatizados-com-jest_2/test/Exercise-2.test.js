8c76acbfd9b4e2fe16525a105df05a852aeb957d
Patrick Dack <patrickdack89@gmail.com>
2021-06-16 03:04:07 -0300

    Testa erros utilizando Promise e async/await para Exercicios 2 e 3

--- exercises/testes-automatizados-com-jest_2/test/Exercise-2.test.js
+++ exercises/testes-automatizados-com-jest_2/test/Exercise-2.test.js
@@ -2,14 +2,24 @@ const getUserName = require('../source/Exercise-2');
 
 describe('Testa a função getUserName', () => {
   it('testa a função utilizando Promise', () => {
+    expect.assertions(1);
     return getUserName(4)
-    .then((user) => {
-      expect(user).toBe('Mark');
-    });
+    .then((user) => expect(user).toBe('Mark'));
+  });
+   it('testa error utilizando Promise', () => {
+    return getUserName(6)
+    .catch(error => expect(error).toEqual({ error: 'User with ' + 6 + ' not found.' }))
   });
 
   it('testa a função utilizando async/await', async () => {
     const user = await getUserName(5);
     expect(user).toBe('Paul');
+  });
+  it('testa error utilizando async/await', async () => {
+    try {
+      await getUserName(6);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with ' + 6 + ' not found.' });
+    }
   });
 });
