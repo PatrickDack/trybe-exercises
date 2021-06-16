b3c10df4144c78135f136bf746fa11a7cef6ce69
Patrick Dack <patrickdack89@gmail.com>
2021-06-16 04:26:13 -0300

    Exercicio 6.2

--- exercises/testes-automatizados-com-jest_2/test/Exercise-6.test.js
+++ exercises/testes-automatizados-com-jest_2/test/Exercise-6.test.js
@@ -17,7 +17,20 @@ const findAnimalByName = (name) => {
 const getAnimal = (name) => {
   return findAnimalByName(name);
 };
-// ---------------------
+
+const findAnimalByAge = (age) => {
+  return new Promise((resolve, reject) => {
+    const animal = Animals.filter((animals) => animals.age === age);
+    if (animal.length !== 0) {
+      return resolve(...animal);
+    }
+    return reject('Não existe animal com esta idade.');
+  })
+}
+
+const getAnimalByAge = (age) => {
+  return findAnimalByAge(age);
+}
 
 describe('Testando promise - findAnimalByName', () => {
   describe('Quando existe o animal com o nome procurado', () => {
@@ -34,6 +47,26 @@ describe('Testando promise - findAnimalByName', () => {
       expect.assertions(1);
       return getAnimal('Bob').catch(error =>
         expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+describe('Testando promise - findAnimalByAge', () => {
+  describe('Quando existe o animal com a idade procurada', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimalByAge(5).then(animal => {
+        expect(animal).toEqual({ name: 'Preguiça', age: 5, type: 'Cat' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com a idade procurada', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimalByAge(15).catch(error =>
+        expect(error).toEqual('Não existe animal com esta idade.')
       );
     });
   });
