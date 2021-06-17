e7f7fb789dd7e146bf6053de7ce0b0cef7ca6189
Patrick Dack <patrickdack89@gmail.com>
2021-06-17 02:07:21 -0300

    Exercicio 2

--- exercises/testes-automatizados-com-jest_3/test/random.test.js
+++ exercises/testes-automatizados-com-jest_3/test/random.test.js
@@ -1,5 +1,9 @@
 const random = require('../souce/random');
 
+afterEach(() => {
+  random.generate.mockRestore();
+})
+
 it('testa random', () => {
   random.generate = jest.fn().mockReturnValue(23);
   random.generate();
@@ -7,3 +11,17 @@ it('testa random', () => {
   expect(random.generate()).toBe(23);
   expect(random.generate).toHaveBeenCalledTimes(2);
 });
+
+it('testa random com implementation', () => {
+  const randomGen = jest.spyOn(random, 'generate');
+  randomGen.mockImplementation((a, b) => a / b);
+  randomGen();
+  expect(randomGen).toHaveBeenCalled();
+  expect(randomGen(10, 5)).toBe(2);
+  expect(randomGen).toHaveBeenCalledTimes(2);
+
+  randomGen.mockRestore();
+  randomGen.mockReturnValue(54);
+  expect(randomGen()).toBe(54);
+  expect(randomGen).toHaveBeenCalledTimes(1);
+})
