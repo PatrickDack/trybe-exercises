db4b8479d7baf76078e50d8211a2d0bd7b8fd483
Patrick Dack <patrickdack89@gmail.com>
2021-06-17 01:33:47 -0300

    Pre-Aula Exercicio 5

--- exercises/testes-automatizados-com-jest_3/test/math.test.js
+++ exercises/testes-automatizados-com-jest_3/test/math.test.js
@@ -1,11 +1,17 @@
 const math = require('../souce/math');
 jest.mock('../souce/math');
 
-it('testa a função subtrair', () => {
-  math.subtrair = jest.fn().mockReturnValue(5);
-  math.subtrair();
-  expect(math.subtrair).toHaveBeenCalled();
-  expect(math.subtrair(10, 5)).toBe(5);
+beforeEach(() => {
+  math.subtrair.mockRestore();
+});
+
+describe('subtrair', () => {
+  it('testa a função subtrair', () => {
+    math.subtrair = jest.fn().mockReturnValue(5);
+    math.subtrair();
+    expect(math.subtrair).toHaveBeenCalled();
+    expect(math.subtrair(10, 5)).toBe(5);
+  });
 });
 
 it('testa a função multtiplicar', () => {
@@ -29,4 +35,24 @@ it('testa a função dividir', () => {
   expect(math.dividir).toHaveBeenCalled();
   expect(math.dividir(2, 5)).toBe(15);
   expect(math.dividir).toHaveBeenCalledTimes(2);
+});
+
+describe('subtrair com restore', () => {
+  it('testa a função subtrair e a restaura', () => {
+    const mathSub = jest.spyOn(math, 'subtrair');
+    mathSub.mockImplementation((a, b) => a * b);
+    mathSub.mockReturnValue(20);
+
+    expect(mathSub(4, 5)).toBe(20);
+    expect(mathSub).toHaveBeenCalled();
+    expect(mathSub).toHaveBeenCalledTimes(1);
+    expect(mathSub).toHaveBeenCalledWith(4, 5);
+
+    mathSub.mockRestore();
+    mathSub.mockReturnValue(23);
+    expect(mathSub(30, 7)).toBe(23);
+    expect(mathSub).toHaveBeenCalled();
+    expect(mathSub).toHaveBeenCalledTimes(1)
+
+  });
 });
