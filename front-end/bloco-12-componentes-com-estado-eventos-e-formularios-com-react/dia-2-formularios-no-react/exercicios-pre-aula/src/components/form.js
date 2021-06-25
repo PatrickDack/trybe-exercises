c049d2e55d6cf92308a8c0b772eaa6744b6f3c93
Patrick Dack <patrickdack89@gmail.com>
2021-06-25 16:20:05 -0300

    Exercicio 6 - Adicionado tags fildset no formulario

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exercicios-pre-aula/src/components/form.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exercicios-pre-aula/src/components/form.js
@@ -29,24 +29,30 @@ class Form extends React.Component {
       <section>
         <h1>Formulário</h1>
         <form className="form">
-          <label>
+          <fieldset>
+            <legend>Informações Pessoais</legend>
+            <label>
             Nome:
             <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            E-mail:
-            <input name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
-          </label>
-          <br />
+            </label>
+            <br />
+            <label>
+              E-mail:
+              <input name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
+            </label>
+            <br />
 
-          <label>
-            Vai comparecer?
-            <input type="checkbox" name="isGoing" value={this.state.willpresent} onChange={this.handleChange} />
-          </label>
-          <br />
+            <label>
+              Vai comparecer?
+              <input type="checkbox" name="isGoing" value={this.state.willpresent} onChange={this.handleChange} />
+            </label>
+            <br />
+          </fieldset>
 
-          <label>
+          <fieldset>
+            <legend>Preferêcias</legend>
+
+            <label>
             Escolha seu anime favorito:
             <select name="favoriteAnime" value={this.state.favoriteAnime} onChange={this.handleChange}>
             <option value="naruto">Naruto</option>
@@ -54,16 +60,17 @@ class Form extends React.Component {
             <option value="onepiece">One Piece</option>
             <option value="beaststars">BeastStars</option>
             </select>
-          </label>
-          <br />
+            </label>
+            <br />
 
-          <FileInput />
+            <FileInput />
 
-          <label>
-            Deixe um Comentário:
-            <textarea name="coments" value={this.state.coments} onChange={this.handleChange}></textarea>
-          </label>
+            <label>
+              Deixe um Comentário:
+              <textarea name="coments" value={this.state.coments} onChange={this.handleChange}></textarea>
+            </label>
 
+          </fieldset>
         </form>
       </section>
     );
