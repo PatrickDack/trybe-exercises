98a9ca2f7d6661aff78460f9c71e9ca723c74eae
Patrick Dack <patrickdack89@gmail.com>
2021-10-20 05:16:11 -0300

    Exercicio 3 Adivinhar numero aleatorio

---
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/exercicios/sorteio.js
@@ -0,0 +1,21 @@
+const { questionInt, question, keyInSelect } = require('readline-sync');
+
+const  prizeDraw = () => {
+  const chosenNumber = questionInt('Escolha um numero entre 0 e 10. ');
+
+  const randomNumber = () => Math.round(Math.random() * 10);
+
+  const drawnNumber = randomNumber();
+
+  if (chosenNumber === drawnNumber) {
+    console.log('Parabéns, número correto!');
+  } else {
+    console.log(`Opa, não foi dessa vez. O número era ${drawnNumber}`);
+  };
+
+  const playAgain = keyInSelect(['Sim', 'Não'],'Deseja jogar novamente? ');
+
+  if(!playAgain) return prizeDraw();
+};
+
+prizeDraw();
